927222d40e94802f92f7c1642d49e92960326153
Fernando Cutire UTP <fernando.cutire@utp.ac.pa>
2020-07-23 12:44:05 -0500

    Arreglado webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,21 @@
 const path = require("path");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
+/*
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+if (process.env.NODE_ENV == 'test') {
+	require('dotenv').config({ path: '.env.test' });
+} else if (process.env.NODE_ENV == 'development') {
+	require('dotenv').config({ path: '.env.development' });
+}
+
+*/
 module.exports = (env) => {
   const isProduction = env === 'production';
+	const CSSExtract = new MiniCssExtractPlugin({ filename: 'styles.css' });
+  
   return {
     entry: "./src/app.js",
     output: {
@@ -16,12 +30,30 @@ module.exports = (env) => {
           exclude: /node_modules/,
         },
         {
-          test: /\.s?css$/,
-          use: ["style-loader", "css-loader", "sass-loader"],
-        },
-      ],
-    },
-    devtool: isProduction ? 'source-map' :"eval-cheap-module-source-map",
+          test:  /\.s?css$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader
+            },
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+    }],
+  },
+    plugins: [
+      CSSExtract
+  ],
+    devtool: isProduction ? 'source-map' : "inline-source-map",
     devServer: {
       contentBase: path.join(__dirname, "public"),
       historyApiFallback: true,
