b73e82ca8c3892134424e3507b1b6223f3d102b2
Fernando Cutire UTP <fernando.cutire@utp.ac.pa>
2020-07-24 22:04:44 -0500

    Adding Summary Component

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,14 +1,14 @@
-
-import React from 'react';
-import ExpenseList from './ExpensesList';
-import ExpenseListFilters from './ExpenseListFilters';
+import React from "react";
+import ExpenseList from "./ExpensesList";
+import ExpenseListFilters from "./ExpenseListFilters";
+import ExpensesSummary from './ExpensesSummary';
 
 const ExpenseDashboardPage = () => (
   <div>
+    <ExpensesSummary />
     <ExpenseListFilters />
     <ExpenseList />
   </div>
 );
-
 
 export default ExpenseDashboardPage;
